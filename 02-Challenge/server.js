c7f0664cebb6ddfd8270246b15098bf38e6e9fdb
Ryan Gustafson <ryangustafson@ryans-mac-mini.lan>
2022-10-17 14:22:40 -0500

    added updated port and json package file

--- 02-Challenge/server.js
+++ 02-Challenge/server.js
@@ -6,7 +6,7 @@ const userData = require('./db/db.json');
 const uuid = require('uuid');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // app.use for develop folder
 
